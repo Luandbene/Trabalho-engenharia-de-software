cca77e1d5acda417060b61ef64401860b776d26e
Luandbene <luandibene@gmail.com>
2026-05-11 20:43:07 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,29 @@
-console.log("Sistema iniciado");
+function addTask() {
+
+  const input = document.getElementById("taskInput");
+
+  const taskText = input.value;
+
+  if (taskText === "") {
+    alert("Digite uma tarefa");
+    return;
+  }
+
+  const li = document.createElement("li");
+
+  li.textContent = taskText;
+
+  const removeButton = document.createElement("button");
+
+  removeButton.textContent = "Remover";
+
+  removeButton.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(removeButton);
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
